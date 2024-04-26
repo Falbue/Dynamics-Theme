9480db486ba38bd23903ba8c70fdd8a237068694
Falbue <cyansair05@gmail.com>
2024-04-26 17:24:21 +0500

    функция изменения темы

--- main.py
+++ main.py
@@ -5,3 +5,27 @@ import tkinter as tk
 import winreg
 
 app_name = 'Dynamics Theme'
+
+def set_windows_theme(theme): # изменение темы
+    try:
+        # Открываем ключ реестра для тем
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize", 0, winreg.KEY_WRITE)
+        
+        # Устанавливаем тему в соответствии с выбором
+        if theme == "light":
+            winreg.SetValueEx(key, "AppsUseLightTheme", 0, winreg.REG_DWORD, 1)
+            winreg.SetValueEx(key, "SystemUsesLightTheme", 0, winreg.REG_DWORD, 1)
+            icon.icon = Image.open("lib/icon_light.png")  # Изменяем иконку на светлую
+        elif theme == "dark":
+            winreg.SetValueEx(key, "AppsUseLightTheme", 0, winreg.REG_DWORD, 0)
+            winreg.SetValueEx(key, "SystemUsesLightTheme", 0, winreg.REG_DWORD, 0)
+            icon.icon = Image.open("lib/icon_dark.png")  # Изменяем иконку на тёмную
+        else:
+            print("Некорректная тема. Выберите 'light' или 'dark'.")
+            return False
+        
+        winreg.CloseKey(key)
+        return True
+    except Exception as e:
+        print("Ошибка при установке темы:", e)
+        return False
