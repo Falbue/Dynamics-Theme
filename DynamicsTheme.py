87f144d8faca7dad41786f6a7e2e1d3e6577a5e1
Falbue <cyansair05@gmail.com>
2024-07-07 10:19:25 +0500

    Добавлена новая функция, для определения языка устройства

--- DynamicsTheme.py
+++ DynamicsTheme.py
@@ -32,6 +32,19 @@ def set_windows_theme(theme): # изменение темы
         print("Ошибка при установке темы:", e)
         return False
 
+# Функция для получения текущего языка системы
+def get_system_language():
+    user32 = ctypes.WinDLL('user32', use_last_error=True)
+    kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+
+    LOCALE_NAME_MAX_LENGTH = 85
+
+    locale_name = ctypes.create_unicode_buffer(LOCALE_NAME_MAX_LENGTH)
+    user32.GetUserDefaultLocaleName(locale_name, LOCALE_NAME_MAX_LENGTH)
+    lang = locale_name.value[:2]
+
+    return lang
+
 def get_current_theme(): # получение текущей темы
     try:
         with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize", 0, winreg.KEY_READ) as key:
